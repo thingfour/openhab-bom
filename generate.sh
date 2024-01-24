7c468456422140424bf57275bfae7d672a746420
Łukasz Dywicki <luke@code-house.org>
2024-01-24 23:50:02 +0100

    Initial commit.
    Signed-off-by: Łukasz Dywicki <luke@code-house.org>

---
+++ generate.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+version=$1
+
+if [[ -z "$version" ]]; then
+  echo "Usage ./generate.sh <version>"
+  echo "Be aware - script will change current git branch!"
+  exit 1
+fi
+
+mvn versions:set -DnewVersion="${version}-SNAPSHOT" -DgenerateBackupPoms=false
+mvn versions:update-property -Dproperty=openhab-core.version -DnewVersion="${version}" -DgenerateBackupPoms=false -N
+mvn clean install && mvn clean install
